0fbe4d2ab8c1ba6cd319df96becbbb99417aaf02
Arpit Jain <arpitj938@gmail.com>
2017-12-19 16:23:57 +0530

    mumrical data analysis done

--- housing_problem.py
+++ housing_problem.py
@@ -19,3 +19,49 @@ test = pd.read_csv('test.csv')
 
 print train.head()
 print test.head()
+
+#no of  rows 1460 no of col 81 
+print train.shape[0]
+
+#no of col having empty values 
+
+print train.columns[train.isnull().any()]
+
+#missing value counts in each of these columns
+miss = train.isnull().sum()/len(train)
+miss = miss[miss > 0]
+miss.sort_values(inplace=True)
+print miss
+
+miss = miss.to_frame()
+miss.columns = ['count']
+miss.index.names = ['Name']
+miss['Name'] = miss.index
+
+#plot the missing value count
+#sns.set(style="whitegrid", color_codes=True)
+#sns.barplot(x = 'Name', y = 'count', data=miss)
+#plt.xticks(rotation = 90)
+#sns.distplot(train['SalePrice'])
+
+#skewness of target variable 
+target = np.log(train['SalePrice'])
+print ('Skewness is', target.skew())
+#sns.distplot(target)
+
+#separate variables into new data frames
+numeric_data = train.select_dtypes(include=[np.number])
+cat_data = train.select_dtypes(exclude=[np.number])
+print ("There are {} numeric and {} categorical columns in train data".format(numeric_data.shape[1],cat_data.shape[1]))
+
+del numeric_data['Id']
+
+#correlation plot
+corr = numeric_data.corr()
+#sns.heatmap(corr)
+
+
+print corr['SalePrice'].sort_values(ascending=False)[:28]
+
+
+
